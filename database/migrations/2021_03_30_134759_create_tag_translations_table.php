8e08b9b7fadc6d05ac8b85e943a41d34d46d05f7
antunderek <62905719+antunderek@users.noreply.github.com>
2021-03-30 15:51:17 +0200

    initial commit

---
+++ database/migrations/2021_03_30_134759_create_tag_translations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_translations');
+    }
+}
