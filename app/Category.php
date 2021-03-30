0d07046b62a76c21218a5fbb69d8ca26077ecae8
antunderek <62905719+antunderek@users.noreply.github.com>
2021-03-30 17:09:35 +0200

    add models

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use Translatable;
+
+    public $translatedAttributes = ['title', 'lang_id'];
+
+    public function meals() {
+        return $this->hasMany(Meal::class);
+    }
 }
--- app/Ingredient.php
+++ app/Ingredient.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-    //
+    use Translatable;
+
+    public $translatedAttributes = ['title', 'lang_id'];
+
+    public function meals() {
+        return $this->hasMany(Meal::class);
+    }
 }
