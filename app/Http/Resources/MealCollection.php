ebe6a40ca442726f2461615647679ddc77ba4e84
antunderek <62905719+antunderek@users.noreply.github.com>
2021-04-11 17:52:20 +0200

    fix response metadata

--- app/Http/Resources/MealCollection.php
+++ app/Http/Resources/MealCollection.php
@@ -6,6 +6,28 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class MealCollection extends ResourceCollection
 {
+    private $metaData;
+    private $links;
+
+    public function __construct($resource)
+    {
+        $this->metaData = [
+            'currentPage' => $resource->currentPage(),
+            'totalItems' => $resource->total(),
+            'itemsPerPage' => $resource->perPage(),
+            'totalPages' => $resource->lastPage()
+        ];
+
+        $this->links = [
+            'prev' => $resource->previousPageUrl(),
+            'next' => $resource->nextPageUrl(),
+            'self' => $resource->url($resource->currentPage()),
+        ];
+
+        $resource = $resource->getCollection();
+        parent::__construct($resource);
+    }
+
     /**
      * Transform the resource collection into an array.
      *
@@ -14,9 +36,11 @@ class MealCollection extends ResourceCollection
      */
     public function toArray($request)
     {
+        //dd($this->resource);
         return [
-            'meta' => [],
+            'meta' => $this->metaData,
             'data' => MealResource::collection($this->collection),
+            'links' => $this->links,
         ];
     }
 }
